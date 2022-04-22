674c129b09a3e7aa7f347c574c3ba056599595dd
Robert <r.walerczak@gmail.com>
2022-04-22 14:11:30 +1000

    Tweak player token size

--- client/src/components/atoms/PlayerToken/index.tsx
+++ client/src/components/atoms/PlayerToken/index.tsx
@@ -4,8 +4,8 @@ const PlayerToken = ({style, fill}: any) => {
     console.log(style)
     return (
         <div className='player-token' style={style}>
-            <svg xmlns="http://www.w3.org/2000/svg" width='20' height='50' version="1.1" fill={fill} >
-            <path d="M 10 18 A 1 1 0 0 0 10 0 A 1 1 0 0 0 10 18 C 0 18 0 18 0 44 C 0 50 0 50 5 50 L 15 50 C 20 50 20 50 20 44 C 20 18 20 18 10 18"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width='24' height='47' version="1.1" fill={fill} >
+            <path d="M 12 18 A 1 1 0 0 0 12 0 A 1 1 0 0 0 12 18 L 8 17 C 0 18 0 18 0 41 C 0 45 2 47 5 47 L 19 47 C 22 47 24 45 24 41 C 24 18 24 18 16 17"/>
             </svg>
         </div>
     )
