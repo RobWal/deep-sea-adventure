060e2cadd08cc5bf3bfaa239ed9a4e9e4e2446e4
Robert <r.walerczak@gmail.com>
2025-02-14 15:05:08 +1100

    Add movement animations to oxygen bubble and player tokens.

--- client/src/components/atoms/OxygenMarker/index.tsx
+++ client/src/components/atoms/OxygenMarker/index.tsx
@@ -1,6 +1,8 @@
+import './OxygenMarker.css';
+
 const OxygenMarker = ({style}: any) => {
     return(
-        <div style={style}>
+        <div className='oxygen-marker' style={style}>
             <svg xmlns="http://www.w3.org/2000/svg" width='28' height='28' version="1.1" fill="#FF1616">
             <circle cx="7" cy="7" r="7"/></svg>
         </div>
