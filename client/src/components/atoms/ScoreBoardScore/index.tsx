ec76572d99a4d8dceece2576c0da38b16f0df567
Robert W <r.walerczak@gmail.com>
2025-05-16 13:07:50 +1000

    Alter the scoreboard so that the score is omitted until the final round for scoring.

--- client/src/components/atoms/ScoreBoardScore/index.tsx
+++ client/src/components/atoms/ScoreBoardScore/index.tsx
@@ -1,8 +1,24 @@
-import './ScoreBoardScore.css'
+import './ScoreBoardScore.css';
+import {
+    GameContext,
+} from "../../../application-context";
+import { useContext } from 'react';
+
 
 const ScoreBoardScore = ({style, score}: any) => {
+    const [appState, appAction] = useContext(GameContext);
+    const currentRound = appState.currentRound === 4;
+    const placeHolderScore = '';
+
     return (
-        <div className='score-board-score' style={style}>{score}</div>
+        <div className='score-board-score' style={style}>
+            {
+                currentRound ? 
+                score 
+                : 
+                placeHolderScore
+            }
+        </div>
     )
 }
 
