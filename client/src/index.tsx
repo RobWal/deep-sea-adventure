f17620dc0a47543f88ead1bb53a9305e9fcc9b4d
Robert W <r.walerczak@gmail.com>
2025-06-11 11:00:00 +1000

    Revert homescreen naming changes to the version that successfully compiles.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import GameContainer from './components/pages/GameContainer';
-import Homescreen from './components/pages/Homescreen';
 
 
 const root = ReactDOM.createRoot(
